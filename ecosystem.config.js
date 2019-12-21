37ce61e156af45935eea5bede5f4d55d89fe70fe
qinghe <david@getstore.cn>
2019-12-21 17:43:17 +0800

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,25 @@
 module.exports = {
   apps : [{
-    name: 'zgame',
+    name: '小邦',
     script: 'server/main.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: '',
-    instances: 2,
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '300M',
-    instance_var: 'ZGAME_INSTANCE_ID',
+    instance_var: 'XIAOBANG_INSTANCE_ID',
     env: {
       NODE_ENV: 'production'
     },
     env_staging: {
       NODE_ENV: 'staging'
-    } 
+    }
   }],
 
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
